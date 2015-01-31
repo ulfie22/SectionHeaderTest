9a4ab4099202551bb3e654f57864e962b3c4387c
Andy Drexler <andy@drexler.com>
2015-01-31 12:31:27 -0800

    Initial Commit

---
+++ SectionHeaderTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SectionHeaderTest
+//
+//  Created by Andy Drexler on 1/31/15.
+//  Copyright (c) 2015 NetSMART Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SectionHeaderTestTests/SectionHeaderTestTests.m
@@ -0,0 +1,40 @@
+//
+//  SectionHeaderTestTests.m
+//  SectionHeaderTestTests
+//
+//  Created by Andy Drexler on 1/31/15.
+//  Copyright (c) 2015 NetSMART Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SectionHeaderTestTests : XCTestCase
+
+@end
+
+@implementation SectionHeaderTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
